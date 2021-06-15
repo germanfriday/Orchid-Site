aebb5e7e8810f2de5498fe84c5c1363cb7c5640e
germanfriday <chris@thegermanfriday.com>
2021-06-15 10:01:09 -0400

    complete file upload

---
+++ cgi-bin/webform_referers.pm
@@ -0,0 +1,23 @@
+package webform_referers;
+require Exporter;
+
+our @ISA	= qw(Exporter);
+our @EXPORT	= qw(@referers);
+our @EXPORT_OK	= qw(@referers);
+our @VERSION	= 1.00;
+
+our @referers = (
+	'sf3e238.a2hosting.com',
+	'kissautoauction.com',
+	'www.kissautoauction.com',
+	'motorcityinteractive.com',
+	'www.motorcityinteractive.com',
+	'yahoo.com',
+	'www.yahoo.com',
+	'motorcitydev.com',
+	'www.motorcitydev.com',
+	'orchiddayspa.net',
+	'www.orchiddayspa.net',
+	'orchiddayspa.com',
+	'www.orchiddayspa.com'
+	);
